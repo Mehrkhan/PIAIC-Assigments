60c2f46e8328f34c36d6a201322e3fc00a27a7aa
Mehrkhan <139866560+Mehrkhan@users.noreply.github.com>
2023-08-06 12:40:30 +0500

    gradeCalculation

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Mehran khan");
+function calculategrades() {
+    var promptsync = require("prompt-sync")();
+    var marks = promptsync("Enter Your Marks:");
+    if (marks >= 90 && marks <= 100) {
+        return "Grade A";
+    }
+    else if (marks >= 80 && marks < 90) {
+        return "Grade B";
+    }
+    else if (marks >= 50 && marks < 60) {
+        return "Grade F";
+    }
+    else {
+        return "Invalid";
+    }
+}
+var grade = calculategrades();
+console.log("Your grade is:".concat(grade));
